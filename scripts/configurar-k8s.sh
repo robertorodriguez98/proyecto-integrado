589529b49df87e29061623c1848277ad0655c47b
Roberto Rodríguez Márquez <robertorodriguezmarquez98@gmail.com>
2023-05-23 13:09:13 +0200

    añadido script

---
+++ scripts/configurar-k8s.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+KUBECONFIG=$(kubectl --namespace crossplane-system get secret proyecto-eks-cluster --output jsonpath="{.data.kubeconfig}" | base64 -d)
+kubectl -n crossplane-system create secret generic cluster-config --from-literal=kubeconfig="${KUBECONFIG}"
+echo "$KUBECONFIG"
+kubectl apply -f ../config-kubernetes.yaml
