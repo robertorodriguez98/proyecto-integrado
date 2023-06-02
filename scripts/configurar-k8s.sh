a41e9609efe8b41424337e3032bf94cc0f5e674d
Roberto Rodríguez Márquez <robertorodriguezmarquez98@gmail.com>
2023-06-02 08:32:16 +0200

    nodo eliminado

--- scripts/configurar-k8s.sh
+++ scripts/configurar-k8s.sh
@@ -2,5 +2,5 @@
 
 KUBECONFIG=$(kubectl --namespace crossplane-system get secret proyecto-eks-cluster --output jsonpath="{.data.kubeconfig}" | base64 -d)
 kubectl -n crossplane-system create secret generic cluster-config --from-literal=kubeconfig="${KUBECONFIG}"
-echo "$KUBECONFIG"
+#echo "$KUBECONFIG"
 kubectl apply -f ../config-kubernetes.yaml
